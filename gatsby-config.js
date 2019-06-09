e09d06fd51ca677e34bbed45fdb1aff04c4bc419
mikeybell <michaelrobertbell@gmail.com>
2019-06-09 18:26:15 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mike Bell`,
+    description: `The personal site of Mike Bell`,
+    author: `@mikeybell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
