1929eaff4383055a6db82a67920f5f3a80923521
mikeybell <michaelrobertbell@gmail.com>
2019-07-04 21:30:59 -0400

    Adds image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Content from '../components/content';
+import Image from '../components/image';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Mike Bell</h1>
     <Content />
+    <Image />
   </Layout>
 );
 
