de20f033f7fe69861a7f3f5f35eedae649d13821
mikeybell <michaelrobertbell@gmail.com>
2019-07-04 21:42:39 -0400

    Creates header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Header from '../components/header';
 import Content from '../components/content';
 import Image from '../components/image';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Mike Bell</h1>
+    <Header />
     <Content />
     <Image />
   </Layout>
