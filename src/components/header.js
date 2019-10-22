37109592a9ed79ab9082c76b045d99e84d22af7d
Mike Bell <michaelrobertbell@gmail.com>
2019-10-22 18:05:51 -0400

    Updates content

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import headerStyles from '../styles/header.module.css';
+
+const { name, title, formerTitle } = headerStyles;
 
 const Header = () => (
     <header>
-        <h1>Mike Bell</h1>
+        <h1 className={name}>Mike Bell</h1>
+        <h2 className={title}>
+            Frontend Developer <span className={formerTitle}>(Former Designer)</span>
+        </h2>
+
     </header>
 );
 
